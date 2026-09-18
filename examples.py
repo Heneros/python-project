94d5ae9a6cb652c742020dc3eed96c6efa8808b1
Rustam <punk60826@gmail.com>
2026-09-18 21:38:39 +0300

    small task

--- examples.py
+++ examples.py
@@ -1,6 +1,12 @@
-favorite_languages = {
-      'jen': ['python', 'rust'],
-      'sarah': ['c'],
-      'edward': ['rust', 'go'],
-      'phil': ['python', 'haskell'],
-      }
+from pathlib import Path
+
+contents = "I love programming.\n"
+contents += "I love creating new games.\n"
+contents += "I also love working with data.\n"
+
+
+path = Path('pi_digits.txt')
+# path.write_text(contents)
+
+with open(path, 'a') as fileobj:
+     fileobj.write(contents)
